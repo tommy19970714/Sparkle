82e2b351b60cc6dcc1af4740c111329256d1e3bc
toshiki tomihira <toshiki19970714@gmail.com>
2020-09-14 20:51:35 +0900

    Fix url of Swift Package

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let version = "1.24.0"
 let checksum = "2b6fec0cad2bb643ed222b708f37bf9e98f5ed84d77c9ba69e66dd2b82b0fffa"
-let url = "https://github.com/sparkle-project/Sparkle/releases/download/\(version)/Sparkle-SPM-\(version).zip"
+let url = "https://github.com/tommy19970714/Sparkle/archive/1.24.0-a.zip"
 
 let package = Package(
     name: "Sparkle",
